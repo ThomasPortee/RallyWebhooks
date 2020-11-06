7c2a927b2309dad8af96ff2efb14ccfcab22355e
Carlos Munoz <jose.munoz2@coxautoinc.com>
2020-11-06 09:18:59 -0600

    Added Script for update Target on Webhook: Defect Close Declined Change

---
+++ scripts/updateTargetDefectClosedDeclinedChange.js
@@ -0,0 +1,33 @@
+const request = require("request");
+const utils = require("../app/utils.js");
+//const targetUrl = utils.getTargetUrl();
+
+//const targetUrl ="https://o8fki03ts0.execute-api.us-east-1.amazonaws.com/dev/3277c954-e5fb-11e7-80c1-9a914cz093ae/1.1.3" // AWS Cox environemnt
+// const targetUrl = "https://2p316jv2tk.execute-api.us-east-2.amazonaws.com/dev/3277c954-e5fb-11e7-80c1-9a914cz093ae/1.1.3"; // JCM  environment for testing
+
+var options = {
+	method: "PATCH",
+	url:
+		"https://rally1.rallydev.com/apps/pigeon/api/v2/webhook/73fa2964-2e9c-4daa-8b83-842d4f0e425c", // The URL for the Defect Closed Declined Change Webhook on Training environemnt
+	headers: {
+		"content-type": "application/json",
+		cookie: `ZSESSIONID=${process.env.WEBHOOK_RALLY_API_KEY}`
+	},
+	body: {
+		AppUrl: targetUrl,
+		TargetUrl: targetUrl
+	},
+	json: true
+};
+
+request(options, function (error, response, body) {
+	if (error) throw new Error(error);
+	console.log(`Updated webhook: ${body._ref} (${body.Name})`);
+	var nyc = body;
+	var propValue;
+	for (var propName in nyc) {
+		propValue = nyc[propName];
+
+		console.log(propName, propValue);
+	}
+});
