08fe1b40881c201623890b40a68229c28c63f4fb
CarlosMunoz <jose.munoz2@coxautoinc.com>
2022-09-29 15:59:30 -0500

    Changed to get the URL with getutil

--- scripts/update_webhook_Portfolio_Item_Investment_Category_Changed.js
+++ scripts/update_webhook_Portfolio_Item_Investment_Category_Changed.js
@@ -1,12 +1,13 @@
 const request = require("request");
 const utils = require("../app/utils.js");
-//const targetUrl = utils.getTargetUrl();
+const targetUrl = utils.getTargetUrl();
 
-//const targetUrl ="https://o8fki03ts0.execute-api.us-east-1.amazonaws.com/dev/3277c954-e5fb-11e7-80c1-9a914cz093ae/1.1.3" // AWS Cox environemnt
-//const targetUrl = "https://jdy3dk37sf.execute-api.us-east-1.amazonaws.com/dev/3277c954-e5fb-11e7-80c1-9a914cz093ae/1.1.3"; // JCM  environment for testing
+//const targetUrl ="https://o8fki03ts0.execute-api.us-east-1.amazonaws.com/dev/3277c954-e5fb-11e7-80c1-9a914cz093ae/1.1.3" 
+// AWS Cox  dev environemnt
+//const targetUrl = "https://pmgy8b70wb.execute-api.us-east-1.amazonaws.com/dev/3277c954-e5fb-11e7-80c1-9a914cz093ae/1.1.3"; // JCM  environment for testing
 
 // NGROK target for testing
-const targetUrl = "https://5cb4-2806-2f0-92e4-9cd5-9e70-54de-4951-f0d9.ngrok.io/dev/3277c954-e5fb-11e7-80c1-9a914cz093ae/1.1.3";
+//const targetUrl = "https://5cb4-2806-2f0-92e4-9cd5-9e70-54de-4951-f0d9.ngrok.io/dev/3277c954-e5fb-11e7-80c1-9a914cz093ae/1.1.3";
 
 // webhook url for TRAINNING
 //https://rally1.rallydev.com/apps/pigeon/api/v2/webhook/b215a6f5-e572-4d4c-8e85-08c39b908c15
--- scripts/update_webhook_Update_New_Portfolio_Item_Investment_Category.js
+++ scripts/update_webhook_Update_New_Portfolio_Item_Investment_Category.js
@@ -1,12 +1,12 @@
 const request = require("request");
 const utils = require("../app/utils.js");
-//const targetUrl = utils.getTargetUrl();
+const targetUrl = utils.getTargetUrl();
 
 //const targetUrl ="https://o8fki03ts0.execute-api.us-east-1.amazonaws.com/dev/3277c954-e5fb-11e7-80c1-9a914cz093ae/1.1.3" // AWS Cox environemnt
-//const targetUrl = "https://jdy3dk37sf.execute-api.us-east-1.amazonaws.com/dev/3277c954-e5fb-11e7-80c1-9a914cz093ae/1.1.3"; // JCM  environment for testing
+//const targetUrl = "https://pmgy8b70wb.execute-api.us-east-1.amazonaws.com/dev/3277c954-e5fb-11e7-80c1-9a914cz093ae/1.1.3"; // JCM  environment for testing
 
 // NGROK target for testing
-const targetUrl = "https://5cb4-2806-2f0-92e4-9cd5-9e70-54de-4951-f0d9.ngrok.io/dev/3277c954-e5fb-11e7-80c1-9a914cz093ae/1.1.3";
+//const targetUrl = "https://5cb4-2806-2f0-92e4-9cd5-9e70-54de-4951-f0d9.ngrok.io/dev/3277c954-e5fb-11e7-80c1-9a914cz093ae/1.1.3";
 
 // webhook url for TRAINNING
 //https://rally1.rallydev.com/apps/pigeon/api/v2/webhook/8071d783-2f50-45e7-b460-5a3098f0e9c3 
